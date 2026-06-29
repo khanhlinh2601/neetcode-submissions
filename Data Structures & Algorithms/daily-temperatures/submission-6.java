9d58c7728257987126d10afd2373e5b8742bb737
Trịnh Khánh Linh <78696346+khanhlinh2601@users.noreply.github.com>
2026-06-29 10:18:29 +0700

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+     public static int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int top = stack.pop();
+                res[top] = i - top;
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
