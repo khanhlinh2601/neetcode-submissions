302999c51e65ef35db42435bf727ea74e248554d
Trịnh Khánh Linh <78696346+khanhlinh2601@users.noreply.github.com>
2026-06-29 09:55:48 +0700

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            for (int j = i+1; j < temperatures.length; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    res[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
